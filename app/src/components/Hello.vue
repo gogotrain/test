8cac2a26d89ee5fdc96ace0921ce5bf0b984353c
gogotrain <31063056+gogotrain@users.noreply.github.com>
2017-08-19 14:18:49 +0800

    origin

--- app/src/components/Hello.vue
+++ app/src/components/Hello.vue
@@ -8,7 +8,6 @@
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
     
     <ul>
